e5d1fe8143f72df0c5b3d9d3f58f847f26043338
Jhonatan Soto <jhonrocker2012@gmail.com>
2023-02-23 21:47:28 -0400

    initial structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PagesModule } from './pages/pages.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        PagesModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/pages/main-page/main-page.component.ts
@@ -0,0 +1,10 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  encapsulation: ViewEncapsulation.None
+})
+export class MainPageComponent {
+
+}
