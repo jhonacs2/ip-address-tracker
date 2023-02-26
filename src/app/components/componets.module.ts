5b6b4fcef1a9a05a7005dff32d9094536724c635
Jhonatan Soto <jhonrocker2012@gmail.com>
2023-02-26 12:17:21 -0400

    add map leaflet js

--- src/app/components/componets.module.ts
+++ src/app/components/componets.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ItSearchComponent } from './it-search/it-search.component';
-
+import { ItMapComponent } from './it-map/it-map.component';
 
 
 @NgModule({
-    declarations: [
-        ItSearchComponent
-    ],
-    exports: [
-        ItSearchComponent
-    ],
-    imports: [
-        CommonModule
-    ]
+  declarations: [
+    ItSearchComponent,
+    ItMapComponent
+  ],
+  exports: [
+    ItSearchComponent,
+    ItMapComponent
+  ],
+  imports: [
+    CommonModule
+  ]
 })
-export class ComponetsModule { }
+export class ComponetsModule {
+}
